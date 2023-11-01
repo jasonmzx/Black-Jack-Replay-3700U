86c9fcf97d70d7ed33c1923a42dd0ed64179e70b
Jason Manarroo <68576119+jasonmzx@users.noreply.github.com>
2023-11-01 15:56:35 -0400

    Robustified Login, added cookie functionaltiy

--- API/source/auth/endpoints.py
+++ API/source/auth/endpoints.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from database import connection_pool
 from .models import SignUpForm, LoginForm
 
-#Import python's builtin hashing lib
+#python's builtin hashing lib
 import hashlib
+
+#python's builtin uuid generator lib
+import uuid
 
 router = APIRouter()
 
@@ -15,6 +18,10 @@ def signup(req: SignUpForm):
 
     plaintext_password = req.plaintext_password
     hashed_password = hashlib.sha256(plaintext_password.encode()).hexdigest()
+
+    #TODO: Replace with with statement, and update error handling
+    #TODO: check for duplicate usernames upon creation
+    #TODO: more assertions you find nessarry, revamp needed
 
     try:
         # Insert new user
@@ -41,25 +48,41 @@ def signup(req: SignUpForm):
 
 @router.post("/login")
 def login(req: LoginForm):
-    conn = connection_pool.get_conn()
-    cursor = conn.cursor(dictionary=True)
 
     plaintext_password = req.plaintext_password
     hashed_password = hashlib.sha256(plaintext_password.encode()).hexdigest()
 
     try:
+        #* With closes connection pool automatically!
 
-        sql = ("SELECT * FROM users WHERE password_hash = %s ")
-        val = (hashed_password,) # Val needs to be a tuple, that's a minimum of 2 values (hence the extra comma)
-        cursor.execute(sql,val)
-        
-        results = cursor.fetchall()
-        print(results)
-    except Exception as err:
+        with connection_pool.get_conn() as conn, conn.cursor(dictionary=True) as cursor:
+
+            # Find the user by username and password hash.
+            sql = "SELECT * FROM users WHERE username = %s AND password_hash = %s"
+            val = (req.username, hashed_password)
+            cursor.execute(sql, val)
+            
+            result = cursor.fetchone()
+
+            # Access will be denied if the user isn't found or if the password isn't correct.
+            if result is None:
+                raise HTTPException(status_code=401, detail="Access Denied")
+
+            # User successfully logged in, so generate a cookie for them
+            new_cookie = str(uuid.uuid4())
+
+            # Update the active_cookie column for the user.
+            sql = "UPDATE users SET active_cookie = %s WHERE username = %s"
+            val = (new_cookie, req.username)
+            cursor.execute(sql, val)
+
+            conn.commit() #As it's an update stmt
+
+            return {"cookie": new_cookie}
+
+    except HTTPException as http_err:  # Catch explicit HTTPExceptions (FastAPI Error API Response)
+        raise http_err
+
+    except Exception as err:  # Catch all other errors
         print(f"Error: {err}")
-        return {"Error": str(err)}
-    finally:
-        cursor.close()
-        conn.close()
-
-    return {"OK!" : req.username}
+        raise HTTPException(status_code=500, detail=str(err)) #Internal serv err
