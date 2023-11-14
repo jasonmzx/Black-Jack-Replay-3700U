cd31c5f67d31dbb387e201ae8401572d45479cab
Tahmid Chowdhury <tahmid1203@hotmail.com>
2023-11-13 20:38:28 -0500

    Resolved issue #1

--- Frontend/blackjack-web/src/components/CardRender.js
+++ Frontend/blackjack-web/src/components/CardRender.js
@@ -8,7 +8,15 @@ const CardRender = ({ shown, card_type, card_name, symbol_type, symbol_name, car
   React.useEffect(() => {
     const importImage = async () => {
       try {
-        const importedImage = await import(`../static/img/${symbol_name}/${card_value}.png`)
+        let imgName
+
+        if (card_name === "Ace" || card_name === "Jack" || card_name === "Queen" || card_name === "King") {
+          imgName = `${card_name}.png`;
+        } else {
+          imgName = `${card_value}.png`;
+        }
+
+        const importedImage = await import(`../static/img/${symbol_name}/${imgName}`)
         setImage(importedImage.default)
       } catch (error) {
         console.error(error)
@@ -18,11 +26,15 @@ const CardRender = ({ shown, card_type, card_name, symbol_type, symbol_name, car
     importImage()
   }, [card_value, symbol_name])
 
+  const formattedValue = card_name === "Ace" || card_name === "Jack" || card_name === "Queen" || card_name === "King" ? card_name : card_value
+  const valueDisplay = card_name === "Ace" ? "1 or 11" : `${card_value}`
+  const cardDescription = card_value === -1 ? "Unknown Card" : `${formattedValue} ${card_name ? `(${valueDisplay}) ` : ''}of ${symbol_name}`
+
   return (
     <div>
       <img src={card_value === -1 ? cardBack : image} width={200} alt="Card Image" />
       <br />
-      {card_value} | {card_name} of {symbol_name}
+      {cardDescription}
     </div>
   )
 }
